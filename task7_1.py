0ae445ff2c80ef23dea11ef86bf68d961adc611d
AlekseyB86 <avbelov86@mail.ru>
2021-08-02 23:16:42 +0300

    init task7_1.py

--- task7_1.py
+++ task7_1.py
@@ -12,35 +12,42 @@ def sort_p(lst: list) -> list:  # Пузырьковая сортировка и
     length = len(lst)
     for k in range(1, length):
         for i in range(length - k):
-            if lst[i] < lst[i + 1]:
+            if lst[i] > lst[i + 1]:
                 lst[i], lst[i + 1] = lst[i + 1], lst[i]
     return lst
 
 
-def sort_p_mod(a: list, direction=True) -> list:  # Пузырьковая сортировка улучшеная версия
+def sort_p_mod(a: list) -> list:  # Пузырьковая сортировка улучшеная версия
     """Модификация пузырьковой сортировки - уменьшено количество проходов"""
-    k = 1
     rev = 1
-    for n in range(len(a) // 2):
+    start = 0
+    tmp = None
+    end = len(a) - 1
+    for _ in range(end + 1):
         if rev > 0:
-            for i in range(len(a) - k):
-                if a[i] > a[i+1] and direction:
-                    a[i], a[i+1] = a[i+1], a[i]
-                else:
-
+            for i in range(start, end):
+                tmp = a[i]
+                if a[i] > a[i + 1]:
+                    a[i], a[i + 1] = a[i + 1], a[i]
+            end -= 2 if tmp == a[end - 1] else 1
         else:
-            for i in range(k, len(a) - k, -1):
-                if a[i] > a[i-1]:
-                    a[i], a[i - 1] = a[i - 1], a[i]
+            for i in range(end - start):
+                tmp = a[end - i]
+                if a[end - i] < a[end - i - 1]:
+                    a[end - i], a[end - i - 1] = a[end - i - 1], a[end - i]
+            start += 2 if tmp == a[end - i] else 1
         rev *= -1
+        if end == 1:
+            break
     return a
 
 
-nums = [randint(-100, 99) for _ in range(10)]
+nums = [randint(-100, 100) for _ in range(1000)]
+# nums = [65, 96, 84, 49, 93, -63, -58, -33, -5, 25]
 print(nums)
 nums_cp = nums.copy()
 
-# cProfile.run('sort_p(nums)')
+cProfile.run('sort_p(nums)')
 print(sort_p(nums))
-# cProfile.run('sort_p_mod(nums_cp)')
+cProfile.run('sort_p_mod(nums_cp)')
 print(sort_p_mod(nums_cp))
