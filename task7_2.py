d9b93d8efe84d5c05ef9636013284d88d489312f
AlekseyB86 <avbelov86@mail.ru>
2021-08-05 20:47:51 +0300

    edited task7_2.py

--- task7_2.py
+++ task7_2.py
@@ -2,7 +2,7 @@
 # заданный случайными числами на промежутке [0; 50).
 # Выведите на экран исходный и отсортированный массивы.
 
-from random import shuffle, random
+from random import random
 import operator
 
 
@@ -11,7 +11,8 @@ def merge_sort(arr: list, compare=True) -> list:
     if len(arr) < 2:
         return arr[:]
 
-    middle = len(arr) // 2  # разделение массива на две части
+    # разделение массива на две части
+    middle = len(arr) // 2
     left = merge_sort(arr[:middle], compare)
     right = merge_sort(arr[middle:], compare)
 
@@ -19,11 +20,12 @@ def merge_sort(arr: list, compare=True) -> list:
 
 
 def merge(left, right, compare) -> list:
-    # сравнивает числа левого и правого и возвращает масив отсортированх чисел
+    # сравнивает числа левого и правого и возвращает массив отсортированх чисел
     result = []
     i, j = 0, 0
     compare = operator.lt if compare else operator.gt
-    # сравнение чисел из левого и парвого и добавление их в новый массив
+
+    # сравнение чисел из левого и правого и добавление их в новый массив
     while i < len(left) and j < len(right):
         if compare(left[i], right[j]):
             result.append(left[i])
@@ -31,13 +33,13 @@ def merge(left, right, compare) -> list:
         else:
             result.append(right[j])
             j += 1
+
     # добавление в конец массива остальных чисел из левого либо правого, которые остались после сравнения
     result.extend(left[i:]) if j > i else result.extend(right[j:])
 
     return result
 
 
-if __name__ == '__main__':
-    nums = [round(50 * (1 - random()), 2) for _ in range(10)]
-    print(f'исходный массив:\n{nums}')
-    print(f'Масиив псле сортировки:\n{merge_sort(nums, False)}')
+nums = [round(50 * (1 - random()), 2) for _ in range(10)]
+print(f'исходный массив:\n{nums}')
+print(f'Масиив псле сортировки:\n{merge_sort(nums, False)}')
