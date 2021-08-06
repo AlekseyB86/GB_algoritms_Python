93ec17cb1890ad3960173e945b8adab957f0fad5
AlekseyB86 <avbelov86@mail.ru>
2021-08-06 22:08:36 +0300

    edited task7_3.py

--- task7_3.py
+++ task7_3.py
@@ -6,7 +6,32 @@
 
 from random import random
 
-# m = int(input('Введите число m: '))
+
+def get_median(lst: list) -> int:
+    """
+    Функция нахождения индекса медианы в массиве
+    :param lst:
+    :return: index median
+    """
+    median = lst[len(lst) // 2]
+    less = [el for el in lst if el < median]
+    greater = [el for el in lst if el > median]
+    delta = len(less) - len(greater)
+    while delta:
+        if delta > 0:
+            greater.append(median)
+            median = max(less)
+            less.remove(max(less))
+        else:
+            less.append(median)
+            median = min(greater)
+            greater.remove(min(greater))
+        delta = len(less) - len(greater)
+
+    return median
+
+
 m = 10
 arr = [int(100 * random()) for _ in range(2 * m + 1)]
 print(arr)
+print(f'Медиана массива: {get_median(arr)}')
